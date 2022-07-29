d5e8a2dca9b2411c52c562451283ca78a393e2ce
yangdengcheng <hongyi@startdt.com>
2022-07-29 10:04:04 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react'
 
+// Styles
+import './common/styles/index.css'
+
+// 以下代码将会报错，最外层不能存在并列的标签。
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Fragment>
+            <div className="App">
+                <h1>This is React App.</h1>
+            </div>
+            <div className="App-other">
+                <h1>This is React App-other.</h1>
+            </div>
+        </Fragment>
+    )
 }
 
-export default App;
+export default App
