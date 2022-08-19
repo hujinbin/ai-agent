817bd3ec8ea9af6add24953a6bf0bfd63658a85d
yangdengcheng <hongyi@startdt.com>
2022-08-19 16:06:40 +0800

    feat: register form

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,6 +1,37 @@
-function RegisterIndex(props) {
+import {Col, Row, message} from "antd";
+import RegisterForm from "./components/RegisterForm";
+import React from "react";
+import {fetchUserRegister} from "../../services/loginServices";
+import { useNavigate } from "react-router-dom";
+import './styles/index.less'
+
+function RegisterIndex() {
+    const navigate = useNavigate();
+    const onFinish = (data) => {
+        const { username, password, code } = data;
+        const params = {
+            username,
+            password,
+            code
+        }
+        fetchUserRegister(params).then(async res => {
+            console.log(res);
+            if (res.data.code === 200) {
+                message.success('注册成功!');
+                navigate('/login');
+            }
+        });
+    }
+
     return (
-        <div>注册</div>
+        <Row className={'register-wrapper'}>
+            <Col span={6} offset={9}>
+                <h4 className={'title'}>Monitor App</h4>
+                <RegisterForm
+                    onFinish={onFinish}
+                />
+            </Col>
+        </Row>
     )
 }
 
