146820c5181ffb144e3a4f3167fd4e85b385a6d4
yangdengcheng <hongyi@startdt.com>
2022-10-17 14:36:06 +0800

    feat: 接口性能数据表格列样式修改

--- src/pages/data/performance/index.js
+++ src/pages/data/performance/index.js
@@ -7,31 +7,38 @@ function DataPerformance() {
             title: 'ID',
             dataIndex: 'Id',
             key: 'Id',
+            width: 70,
         },
         {
             title: '类别',
             dataIndex: 'Kind',
             key: 'Kind',
+            width: 120,
         },
         {
             title: '事件类型',
             dataIndex: 'EventType',
             key: 'EventType',
+            width: 100,
         },
         {
             title: 'URL',
             dataIndex: 'Pathname',
-            key: 'Pathname'
+            key: 'Pathname',
+            ellipsis: true,
         },
         {
             title: '接口状态',
             key: 'Status',
-            render: (record) => <span>{ record.Status }({ record.StatusText || '-' })</span>
+            render: (record) => <span>{ record.Status }({ record.StatusText || '-' })</span>,
+            width: 200,
         },
         {
             title: '接口耗时',
             dataIndex: 'Duration',
-            key: 'Duration'
+            key: 'Duration',
+            render: (record) => <span>{ (record / 1000).toFixed(0) }s</span>,
+            width: 100,
         }
     ];
 
