50527556b85f661cc94605427b76302bae41f07d
yangdengcheng <hongyi@startdt.com>
2022-10-18 10:23:09 +0800

    feat: 修改样式

--- src/pages/data/stable/components/stableCard.js
+++ src/pages/data/stable/components/stableCard.js
@@ -1,8 +1,10 @@
-import {Card, Button} from 'antd';
+import {Card, Button, message} from 'antd';
 import StableInfoDrawer from "./stableInfoDrawer";
 import {STABLE_PAGE_TYPE} from "../../../../enums";
 import {timeConsumingFormatter, calPercent} from "../../../../utils";
 import {useState} from "react";
+import {CopyOutlined} from "@ant-design/icons";
+import CopyToClipboard from "react-clipboardjs-copy";
 
 function StableCard(props) {
     const {data} = props;
@@ -12,61 +14,69 @@ function StableCard(props) {
         setDrawerVisible(status);
     }
 
-    const payloadWrapper = () => (
+    const pageloadWrapper = (data) => (
         <>
             <p className={'card-content-item'}>
                 <span className={'label'}>TCP连接耗时: </span>
-                <span className={'content'}>{timeConsumingFormatter(data.ConnectTime)}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.ConnectTime) || '-'}</span>
             </p>
             <p className={'card-content-item'}>
                 <span className={'label'}>TTFB: </span>
-                <span className={'content'}>{timeConsumingFormatter(data.TtfbTime)}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.TtfbTime) || '-'}</span>
             </p>
             <p className={'card-content-item'}>
                 <span className={'label'}>Response耗时: </span>
-                <span className={'content'}>{timeConsumingFormatter(data.ResponseTime)}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.ResponseTime) || '-'}</span>
             </p>
             <p className={'card-content-item'}>
                 <span className={'label'}>DOM解析渲染耗时: </span>
-                <span className={'content'}>{timeConsumingFormatter(data.ParseDOMTime)}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.ParseDOMTime) || '-'}</span>
             </p>
             <p className={'card-content-item'}>
-                <span className={'label'}>DOMContentLoaded事件回调耗时: </span>
-                <span className={'content'}>{timeConsumingFormatter(data.DomContentLoadedTime)}</span>
+                <span className={'label'}>DOMContentLoaded回调耗时: </span>
+                <span className={'content'}>{timeConsumingFormatter(data.DomContentLoadedTime) || '-'}</span>
             </p>
             <p className={'card-content-item'}>
                 <span className={'label'}>首次可交互时间: </span>
-                <span className={'content'}>{timeConsumingFormatter(data.TimeToInteractive)}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.TimeToInteractive) || '-'}</span>
             </p>
             <p className={'card-content-item'}>
                 <span className={'label'}>页面完整加载时间: </span>
-                <span className={'content'}>{timeConsumingFormatter(data.LoadTime)}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.LoadTime) || '-'}</span>
             </p>
         </>
     );
 
-    const commonWrapper = () => (
+    const commonWrapper = (data) => (
         <>
             <p className={'card-content-item'}>
                 <span className={'label'}>事件类型: </span>
-                <span className={'content'}>{data.EventType}</span>
+                <span className={'content'}>{data.EventType || '-'}</span>
             </p>
             <p className={'card-content-item'}>
                 <span className={'label'}>开始时间: </span>
-                <span className={'content'}>{data.StartTime}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.StartTime) || '-'}</span>
             </p>
             <p className={'card-content-item'}>
                 <span className={'label'}>持续时间: </span>
-                <span className={'content'}>{data.Duration}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.Duration) || '-'}</span>
             </p>
+
             <p className={'card-content-item'}>
                 <span className={'label'}>选中节点: </span>
-                <span className={'content'}>{data.Selector}</span>
+                { data.Selector ? <CopyToClipboard
+                    text={data.Selector}
+                    onSuccess={() => {
+                        message.success('复制成功！');
+                    }}
+                >
+                    <CopyOutlined className={'markdown-copy-icon'}/>
+                </CopyToClipboard> : '-' }
             </p>
         </>
     )
 
-    const memoryWrapper = () => (
+    const memoryWrapper = (data) => (
         <>
             <p className={'card-content-item'}>
                 <span className={'label'}>可使用内存: </span>
@@ -90,9 +100,9 @@ function StableCard(props) {
             className={'stable-card'}
             extra={<Button type={'link'} onClick={() => handleSetDrawerVisible(true)}>查看详情</Button>}
         >
-            {data.Kind === 'payload' ? payloadWrapper() : null}
-            {['longtask', 'resource', 'draw'].includes(data.Kind) ? commonWrapper() : null}
-            {data.Kind === 'memory' ? memoryWrapper() : null}
+            {data.Kind === 'pageload' ? pageloadWrapper(data) : null}
+            {['longtask', 'resource', 'draw'].includes(data.Kind) ? commonWrapper(data) : null}
+            {data.Kind === 'memory' ? memoryWrapper(data) : null}
 
             <StableInfoDrawer
                 data={data}
--- src/pages/data/stable/components/stableInfoDrawer.js
+++ src/pages/data/stable/components/stableInfoDrawer.js
@@ -11,53 +11,53 @@ function StableInfoDrawer(props) {
         data,
     } = props;
 
-    const payloadWrapper = () => (
+    const pageloadWrapper = (data) => (
         <>
             <p className={'drawer-content-item'}>
                 <span className={'label'}>TTFB：</span>
-                <span className={'content'}>{timeConsumingFormatter(data.TtfbTime)}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.TtfbTime) || '-'}</span>
             </p>
             <p className={'drawer-content-item'}>
                 <span className={'label'}>TCP连接耗时：</span>
-                <span className={'content'}>{timeConsumingFormatter(data.ConnectTime)}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.ConnectTime) || '-'}</span>
             </p>
             <p className={'drawer-content-item'}>
                 <span className={'label'}>Response耗时：</span>
-                <span className={'content'}>{timeConsumingFormatter(data.ResponseTime)}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.ResponseTime) || '-'}</span>
             </p>
             <p className={'drawer-content-item'}>
                 <span className={'label'}>DOM解析渲染耗时：</span>
-                <span className={'content'}>{timeConsumingFormatter(data.ParseDOMTime)}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.ParseDOMTime) || '-'}</span>
             </p>
             <p className={'drawer-content-item'}>
-                <span className={'label'}>DOMContentLoaded事件回调耗时：</span>
-                <span className={'content'}>{timeConsumingFormatter(data.DomContentLoadedTime)}</span>
+                <span className={'label'}>DOMContentLoaded回调耗时：</span>
+                <span className={'content'}>{timeConsumingFormatter(data.DomContentLoadedTime) || '-'}</span>
             </p>
             <p className={'drawer-content-item'}>
                 <span className={'label'}>首次可交互时间：</span>
-                <span className={'content'}>{timeConsumingFormatter(data.TimeToInteractive)}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.TimeToInteractive) || '-'}</span>
             </p>
         </>
     );
 
-    const commonWrapper = () => (
+    const commonWrapper = (data) => (
         <>
             <p className={'drawer-content-item'}>
                 <span className={'label'}>事件类型：</span>
-                <span className={'content'}>{data.EventType}</span>
+                <span className={'content'}>{data.EventType || '-'}</span>
             </p>
             <p className={'drawer-content-item'}>
                 <span className={'label'}>页面完整加载时间：</span>
-                <span className={'content'}>{timeConsumingFormatter(data.LoadTime)}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.LoadTime) || '-'}</span>
             </p>
             <p className={'drawer-content-item'}>
                 <span className={'label'}>持续时间：</span>
-                <span className={'content'}>{timeConsumingFormatter(data.Duration)}</span>
+                <span className={'content'}>{timeConsumingFormatter(data.Duration) || '-'}</span>
             </p>
         </>
     );
 
-    const memoryWrapper = () => (
+    const memoryWrapper = (data) => (
         <>
             <p className={'drawer-content-item'}>
                 <span className={'label'}>内存大小限制：</span>
@@ -85,25 +85,27 @@ function StableInfoDrawer(props) {
             visible={drawerVisible}
             onClose={onClose}
         >
-            <p className={'drawer-content-item'}>
-                <span className={'label'}>XPath路径：</span>
-                <CopyToClipboard
-                    text={data.Selector}
-                    onSuccess={() => {
-                        message.success('复制成功！');
-                    }}
-                >
-                    <CopyOutlined className={'markdown-copy-icon'}/>
-                </CopyToClipboard>
-            </p>
+            {
+                data.Selector ? <p className={'drawer-content-item'}>
+                    <span className={'label'}>XPath路径：</span>
+                    <CopyToClipboard
+                        text={data.Selector}
+                        onSuccess={() => {
+                            message.success('复制成功！');
+                        }}
+                    >
+                        <CopyOutlined className={'markdown-copy-icon'}/>
+                    </CopyToClipboard>
+                </p> : null
+            }
             <p className={'drawer-content-item'}>
                 <span className={'label'}>类型：</span>
                 <span className={'content'}>{STABLE_PAGE_TYPE.getName(data.Kind)}</span>
             </p>
 
-            {data.Kind === 'payload' ? payloadWrapper() : null}
-            {['longtask', 'resource', 'draw'].includes(data.Kind) ? commonWrapper() : null}
-            {data.Kind === 'memory' ? memoryWrapper() : null}
+            {data.Kind === 'pageload' ? pageloadWrapper(data) : null}
+            {['longtask', 'resource', 'draw'].includes(data.Kind) ? commonWrapper(data) : null}
+            {data.Kind === 'memory' ? memoryWrapper(data) : null}
 
         </Drawer>
     )
