a27f9abbeea0f57e5bba298ecb3fb839fa54cce1
yangdengcheng <hongyi@startdt.com>
2022-11-17 17:08:50 +0800

    feat: 联调控制台

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -5,91 +5,7 @@ import {useEffect, useState} from "react";
 import {fetchNetWorkErrorOverview, fetchStablerOverview} from "../../services/dashboard";
 import './styles/index.less';
 
-const MOCK_NET_WORK_DATA = [
-    {
-        Url: 'http://www.baidu.com',
-        Value: 1,
-    },
-    {
-        Url: 'http://www.baidu.com',
-        Value: 2,
-    },
-    {
-        Url: 'http://www.baidu.com',
-        Value: 3,
-    },
-    {
-        Url: 'http://www.baidu.com',
-        Value: 4,
-    },
-    {
-        Url: 'http://www.baidu.com',
-        Value: 5,
-    },
-    {
-        Url: 'http://www.baidu.com',
-        Value: 4,
-    },
-    {
-        Url: 'http://www.baidu.com',
-        Value: 3,
-    },
-    {
-        Url: 'http://www.baidu.com',
-        Value: 2,
-    },
-    {
-        Url: 'http://www.baidu.com',
-        Value: 1,
-    },
-    {
-        Url: 'http://www.baidu.com',
-        Value: 1,
-    }
-]
-
-const MOCK_STABLE_DATA = [
-    {
-        Url: 'http://www.qq.com',
-        Value: 1,
-    },
-    {
-        Url: 'http://www.qq.com',
-        Value: 2,
-    },
-    {
-        Url: 'http://www.qq.com',
-        Value: 3,
-    },
-    {
-        Url: 'http://www.qq.com',
-        Value: 4,
-    },
-    {
-        Url: 'http://www.qq.com',
-        Value: 5,
-    },
-    {
-        Url: 'http://www.qq.com',
-        Value: 4,
-    },
-    {
-        Url: 'http://www.qq.com',
-        Value: 3,
-    },
-    {
-        Url: 'http://www.qq.com',
-        Value: 2,
-    },
-    {
-        Url: 'http://www.qq.com',
-        Value: 1,
-    },
-    {
-        Url: 'http://www.qq.com',
-        Value: 1,
-    }
-]
+const DomainId = localStorage.getItem('currentDomain') || '';
 
 const getTopOptions = ({ titleText, data }) => {
     return {
@@ -157,29 +73,21 @@ function Dashboard() {
     const [stableOptions, setStableOptions] = useState({});
 
     useEffect(() => {
-        // fetchNetWorkErrorOverview().then(res => {
-        //     const data = res.data.data || [];
-        //     setNetworkOptions(getNetworkOptions(data));
-        // })
-        setNetworkOptions(getTopOptions({
-            titleText: '网络异常接口TOP 10',
-            data: MOCK_NET_WORK_DATA
-                .sort(
-                    (a, b) => a.Value - b.Value)
-        }))
+        fetchNetWorkErrorOverview({
+            DomainId,
+        }).then(res => {
+            const data = res.data.data || [];
+            setNetworkOptions(getTopOptions(data));
+        })
     }, [])
 
     useEffect(() => {
-        // fetchStablerOverview().then(res => {
-        //     const data = res.data.data || [];
-        //     setStableOptions(getStableOptions(data));
-        // })
-        setStableOptions(getTopOptions({
-            titleText: '网络稳定接口TOP 10',
-            data: MOCK_STABLE_DATA
-                .sort(
-                    (a, b) => a.Value - b.Value)
-        }))
+        fetchStablerOverview({
+            DomainId,
+        }).then(res => {
+            const data = res.data.data || [];
+            setStableOptions(getTopOptions(data));
+        })
     }, [])
 
     return (
--- src/services/dashboard.js
+++ src/services/dashboard.js
@@ -1,17 +1,19 @@
 import request from "../utils/request";
 
 // 获取接口错误TOP10
-export function fetchNetWorkErrorOverview() {
+export function fetchNetWorkErrorOverview(params) {
     return request({
         url: "/report/networkErrorOverview",
         method: "GET",
+        params,
     });
 }
 
 // 获取接口稳定TOP10
-export function fetchStablerOverview() {
+export function fetchStablerOverview(params) {
     return request({
         url: "/report/networkStabilityOverview",
         method: "GET",
+        params,
     });
 }
